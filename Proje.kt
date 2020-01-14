6f2a2eb34bb8ec417f3956e65fe751b7c2ebafc5
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-14 16:49:00 +0300

    Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import java.io.PrintStream
+
+fun main (args : Array<String>)
+{
+
+   val String1="KOTLİN İLE"
+   val String2 ="ANDROİD PROGRAMLAMA"
+   val String3 ="ÖĞRENİYORUM"
+
+   val String4 = "$String1 $String2 $String3"
+
+   print(String4)
+}
